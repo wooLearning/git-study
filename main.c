11b8972f6f3ba3f61b38595a8fc2badc21aae917
wooLearning <wu3643@gmail.com>
2024-11-17 01:16:03 +0900

    FIRST COMMIT

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello Wrold\n");
+    printf("Hello Wrold\n");
+    printf("Hello Wrold\n");
+    printf("Hello Wrold\n");
+    printf("Hello Wrold\n");
+    printf("Hello Wrold\n");
+    return 0;
+}
